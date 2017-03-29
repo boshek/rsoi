28b726f2f8d96f2f1e78f32f67d115a85602e3b3
Sam Albers <sam.albers@gmail.com>
2017-03-29 11:23:28 -0700

    working version of download_enso

--- R/download-enso.R
+++ R/download-enso.R
@@ -1,11 +1,4 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
+
 #
 # Some useful keyboard shortcuts for package authoring:
 #
@@ -13,6 +6,33 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+download_enso <- function() {
+    enso <- read.table("http://www.cpc.ncep.noaa.gov/products/analysis_monitoring/ensostuff/detrend.nino34.ascii.txt", header = TRUE)
+    enso <- dplyr::tbl_df(enso)
+    enso
 }
+
+
+library(tidyverse)
+library(lubridate)
+
+enso %>%
+  mutate(Date = ymd(paste0(YR,"-",MON,"-1"))) %>%
+  filter(YR >= 2003) %>%
+  mutate(variance = ifelse(ANOM > 0,"POS", ifelse(ANOM < 0, "NEG", "NEUTRAL"))) %>%
+  ggplot(aes(x = Date, y = ANOM)) +
+  geom_bar(aes(fill = variance), stat = 'identity') +
+  geom_path() +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y")
+
+
+enso %>%
+  mutate(Date = ymd(paste0(YR,"-",MON,"-1"))) %>%
+  filter(YR >= 2003) %>%
+  group_by(YR) %>%
+  summarise(ANOM = mean(ANOM, na.rm = TRUE)) %>%
+  mutate(variance = ifelse(ANOM > 0,"POS", ifelse(ANOM < 0, "NEG", "NEUTRAL"))) %>%
+  ggplot(aes(x = YR, y = ANOM)) +
+  geom_bar(aes(fill = variance), stat = 'identity') +
+  scale_x_continuous(breaks = seq(2003, 2016, by = 1))
+
