6c0c14d666daa80b59fbb77ef28d66db7ceb1e3a
boshek <sam.albers@gmail.com>
2017-03-31 11:06:52 -0700

    Added data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' SOI index.
+#'
+#'
+#' @format A data frame of SOI
+#' \itemize{
+#' \item Date: Date object that uses the first of the month as a placeholder
+#' \item Month: Month of record
+#' \item Year: Year of record
+#' \item SOI: Southern Oscillation Index
+#' \item SOI_3MON_AVG: 3 Month Average Southern Oscillation Index
+#' \item phase: ENSO phase  
+#' }
+#' @source \url{https://www.ncdc.noaa.gov/teleconnections/enso/indicators/soi/data.csv}
+"soi_df"
