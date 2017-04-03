6d0bada6e0a5aa934e1f1fc3cf371816f779c10e
Sam Albers <sam.albers@gmail.com>
2017-04-03 12:30:46 -0700

    Added test that draw different data source and compares

--- tests/testthat/test_download_soi.R
+++ tests/testthat/test_download_soi.R
@@ -1,18 +1,52 @@
-context("Testing download_soi")
+context("Testing download_enso")
 
 test_that("Does download_soi download a data.frame?", {
-  expect_is( download_soi(), "data.frame" )
+  expect_is( download_enso(), "data.frame" )
 })
 
 
-test_that("Test whether SOI values have changed from February 2017", {
-  data("soi_df")
+test_that("Test whether SOI values have not changed from February 2017", {
+  data("enso")
   
-  soi_df_current <- download_soi()
-  soi_df_current <- soi_df_current[soi_df_current$Date<=max(soi_df$Date),]
+  enso_df_current <- download_enso()
+  enso_df_current <- enso_df_current[enso_df_current$Date<=max(enso$Date),]
 
-  expect_equal( soi_df_current$SOI, 
-                soi_df$SOI
+  expect_equal( enso_df_current$SOI, 
+                enso$SOI
                 )
 })
+
+test_that("Test whether ONI values have not changed from February 2017.", {
+  
+  ## To fix use 
+  #enso <- download_enso()
+  #devtools::use_data(enso)
+  data("enso")
+  
+  enso_df_current <- download_enso()
+  enso_df_current <- enso_df_current[enso_df_current$Date<=max(enso$Date),]
+  
+  expect_equal( enso_df_current$ONI, 
+                enso$ONI
+  )
+})
+
+test_that("Test that download_enso matches most recent value from alternate dataset - 3 months to account for slight data changes",{
+  enso_df_current <- download_enso()
+  
+  ## Most recent from download_enso()
+  recent_enso <- enso_df_current[length(na.omit(enso_df_current$ONI)) + 1 - 3,] #plus one to account for missing first value
+  
+  
+  ## Alternative dataset
+  ## Max year from download_enso -1949 gives last row number
+    oni_df <- read.table("https://www.esrl.noaa.gov/psd/data/correlation/oni.data", nrows = max(enso_df_current$Year)-1949, skip = 1, col.names = c("Year","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"))
+  
+    
+  expect_equal( recent_enso$ONI,
+                oni_df[oni_df$Year==recent_enso$Year, paste0(recent_enso$Month)])
+})
+
+  
+
 
