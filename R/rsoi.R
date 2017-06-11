0b60ccd89d45e753b206381610f573afd2bdde0d
Sam Albers <sam.albers@gmail.com>
2017-06-11 07:37:04 -0700

    Added package-level documentation

---
+++ R/rsoi.R
@@ -0,0 +1,11 @@
+#' \code{rsoi} package
+#'
+#' El Nino/Southern Oscillation (ENSO) and Related Climate Indices
+#'
+#' See the README on
+#' \href{https://cran.r-project.org/web/packages/rsoi/README.html}{CRAN}
+#' or \href{https://github.com/boshek/rsoi/blob/master/README.md}{GitHub}
+#'
+#' @docType package
+#' @name rsoi
+NULL
