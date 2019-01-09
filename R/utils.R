734b0d8a3f34a5d5431586a35d8b39cfd9a0ef50
boshek <sam.albers@gmail.com>
2019-01-09 12:41:15 -0800

    adding check response function

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,21 @@ abbr_month <- function(date){
          levels = c("Jan", "Feb", "Mar", "Apr", "May", 
                     "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"))
 }
+
+## Check the response from server.
+#' @source https://github.com/ropensci/rnoaa/blob/f65d92420e9bb6f4b8d09cbea32c0132638843ce/R/zzz.r#L89-L94
+check_response <- function(response){
+  browser()
+  
+  if(response$status_code == 200){
+    stop(paste0("Non successful http request. Target server returning a ", response$status_code, " error code"),
+        call. = FALSE)
+  }
+  
+  if(grepl("shutdown", response$url)){
+    stop("Data source is currently down due to a government shutdown",
+         call. = FALSE)
+  }
+  
+  invisible(TRUE)
+}
