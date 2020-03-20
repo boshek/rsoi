c90d63e30c1a8465b47dd882312ac7742e8e10fe
Elio Campitelli <eliocampitelli@gmail.com>
2020-03-20 01:12:40 -0300

    Updates AAO documentation (#32)

--- R/download-aao.R
+++ R/download-aao.R
@@ -1,23 +1,23 @@
 #' @export
-#' @title Download Arctic Oscillation data
+#' @title Download Antarctic Oscillation data
 #' 
 #' @inheritParams download_oni
 #' 
-#' @description Projection of the daily 1000 hPa anomaly height field north of 20°N on the first EOF obtained
-#' from the monthly 1000 hPa height anomaly.
+#' @description Projection of the monthly 700 hPa anomaly height field south of 20°S on the first EOF obtained
+#' from the monthly 700 hPa height anomaly.
 #' @return 
 #' \itemize{
+#' \item Date: Date object that uses the first of the month as a placeholder. Date formatted as date on the first of the month because R only supports one partial of date time
 #' \item Month: Month of record
 #' \item Year: Year of record
-#' \item aao: Antarctic Oscillation
+#' \item AAO: Antarctic Oscillation
 #' }
-
 #' @examples
 #' \dontrun{
 #' aao <- download_aao()
 #' }
 #'
-#' @references \url{https://www.cpc.ncep.noaa.gov/products/precip/CWlink/daily_ao_index/aao/aao_index.html}
+#' @references \url{https://www.cpc.ncep.noaa.gov/products/precip/CWlink/daily_ao_index/aao/aao.shtml}
 download_aao <- function(use_cache = FALSE, file = NULL) {
   with_cache(use_cache = use_cache, file = file, 
              memoised = download_aao_memoised, 
@@ -48,7 +48,7 @@ download_aao_unmemoised <- function(){
 
   class(aao) <- c("tbl_df", "tbl", "data.frame") 
   
-  aao[,c("Year","Month", "AAO")]
+  aao[,c("Year", "Month", "Date", "AAO")]
   
 }
 
@@ -61,6 +61,8 @@ read_aao <- function(file) {
   data$Year <- as.integer(data$Year)
   levels <- format(seq(as.Date("2018-01-01"), as.Date("2018-12-01"), "1 month"), "%b")
   data$Month <- factor(data$Month, levels = levels, ordered = TRUE)
+  data$Date <- as.Date(data$Date)
   class(data) <- c("tbl_df", "tbl", "data.frame")
   data
 }
+
